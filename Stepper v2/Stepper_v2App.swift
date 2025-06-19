b85980bc92a3174b3a89e441341c6a73f702fb43
Jon Chan <jchanaf@gmail.com>
2025-06-19 13:14:27 -0700

    Initial Commit

---
+++ Stepper v2/Stepper_v2App.swift
@@ -0,0 +1,19 @@
+//
+//  Stepper_v2App.swift
+//  Stepper v2
+//
+//  Created by Jonathan Chan on 6/19/25.
+//
+
+import SwiftUI
+
+@main
+struct Stepper_v2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
+
---
+++ Stepper v2Tests/Stepper_v2Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Stepper_v2Tests.swift
+//  Stepper v2Tests
+//
+//  Created by Jonathan Chan on 6/19/25.
+//
+
+import Testing
+@testable import Stepper_v2
+
+struct Stepper_v2Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
